fa374037aac628a5986b9e3644868b54484e479d
Isaac Scheff <isaac1996@comcast.net>
2022-11-21 15:21:27 -0500

    Socket connection working

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-const express = require("express");
-const app = express();
+const server = require("express");
+const http = require('http').createServer(server);
+const cors = require('cors');
 
-app.get("/", (req, res) => {
-    res.send("SCserver running");
+const io = require('socket.io')(http, {
+    cors: {
+        origin: 'http://localhost:8080',
+        methods: ["GET", "POST"]
+    }
 });
 
-app.listen(8080, () => {
-    console.log("SCserver listening on port 8080")
+//const app = server();  //const app = express(); (might not be using nay of this)
+
+// app.get("/", (req, res) => {
+//     res.send("SCserver running");
+// });
+
+io.on('connection', function(socket){
+    console.log('User Connected: ' + socket.id);
+});
+
+http.listen(3000, () => {
+    console.log("SCserver listening on port 3000")
 });
 
 
 
-//console.log("node server?");
