889980face8e194fdd3cc633dffde2359c594eea
Isaac Scheff <isaac1996@comcast.net>
2022-11-21 20:57:22 -0500

    Character select connections made

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const server = require("express");
 const http = require('http').createServer(server);
 const cors = require('cors');
 
+let players = {};
+//let playerA = {};
+//let playerB = {};
+let selected = 0;
+let playersReady = 0;
+
 const io = require('socket.io')(http, {
     cors: {
         origin: 'http://localhost:8080',
@@ -9,14 +15,44 @@ const io = require('socket.io')(http, {
     }
 });
 
-//const app = server();  //const app = express(); (might not be using nay of this)
-
-// app.get("/", (req, res) => {
-//     res.send("SCserver running");
-// });
-
 io.on('connection', function(socket){
     console.log('User Connected: ' + socket.id);
+
+    players[socket.id] = {
+        isPlayerA: false,
+        character: null,
+        ready: null
+    };
+
+    if (Object.keys(players).length < 2) {
+        players[socket.id].isPlayerA = true;
+    }
+
+    socket.on('playerReady', function () {
+        console.log("someone's ready");
+        playersReady++;
+        if(playersReady == 2){
+            console.log('Received two readies!');
+            io.emit("selectScreen");
+        }
+    });
+
+    socket.on('disconnect', function () {
+        console.log('A user disconnected: ' + socket.id);
+        delete players[socket.id];
+    });
+
+    socket.on('characterSelect', function (character) {
+        players[socket.id].character = character;
+        console.log(players);
+        selected++;
+        console.log(selected);
+        io.emit("oppoCharacter", character, socket.id);
+    });
+
+    if(selected == 2){
+        //emit player A selection to player B and vice versa
+    }
 });
 
 http.listen(3000, () => {
